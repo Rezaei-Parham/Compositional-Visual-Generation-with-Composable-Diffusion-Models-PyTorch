a3fe7e0e36b7df79e9fe27db9723b48adda70b0e
Rezaei-Parham <cyberrose@protonmail.com>
2024-03-18 21:29:45 +0330

    lmi shape too

--- composable_diffusion/composable_stable_diffusion/pipeline_composable_stable_diffusion.py
+++ composable_diffusion/composable_stable_diffusion/pipeline_composable_stable_diffusion.py
@@ -580,7 +580,7 @@ class ComposableStableDiffusionPipeline(DiffusionPipeline):
 
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
-                print(f"latents: {latents.shape} -><-")
+                print(f"latents: {latents.shape} -><- lmi: {latent_model_input.shape}")
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
                     progress_bar.update()
